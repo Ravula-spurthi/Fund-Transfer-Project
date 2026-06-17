718c903eed076f065d0f082a7a6bb55f6583c5af
Sreeja <baddam.sreeja@accenture.com>
2026-06-17 11:11:44 +0530

    Created login page UI

--- fund-tranfer-ui/src/app/app.ts
+++ fund-tranfer-ui/src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Login } from './components/login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('fund-tranfer-ui');
+
 }
