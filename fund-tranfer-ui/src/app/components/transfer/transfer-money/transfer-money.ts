89c52a0b3dc1f405f23ae5cb3bc9215995e044d6
Bhavana <bachu.bhavana@accenture.com>
2026-06-19 15:32:04 +0530

    2026-06-19: Implemented Add Beneficiary module and Fund Transfer module. Added beneficiary registration form with account validation fields, IFSC and branch details. Developed Transfer Money page with account selection, transfer type, amount, remarks and date fields. Added OTP Verification component and integrated Angular routing between Dashboard, Beneficiary and Fund Transfer screens.

--- fund-tranfer-ui/src/app/components/transfer/transfer-money/transfer-money.ts
+++ fund-tranfer-ui/src/app/components/transfer/transfer-money/transfer-money.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-transfer-money',
-  imports: [],
+  standalone: true,
   templateUrl: './transfer-money.html',
-  styleUrl: './transfer-money.css',
+  styleUrl: './transfer-money.css'
 })
-export class TransferMoney {}
+export class TransferMoney {
+
+  constructor(private router: Router) {}
+
+  transfer() {
+    this.router.navigate(['/otp-verification']);
+  }
+
+  resetForm() {
+    location.reload();
+  }
+}
