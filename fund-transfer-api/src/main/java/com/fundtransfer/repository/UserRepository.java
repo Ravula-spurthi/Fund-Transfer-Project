05fc6de0667880d42ba04d71e41816c91b48dd2d
Bhavana <bachu.bhavana@accenture.com>
2026-06-29 12:34:37 +0530

    Completed User Module APIs with profile retrieval, balance retrieval and profile update

--- fund-transfer-api/src/main/java/com/fundtransfer/repository/UserRepository.java
+++ fund-transfer-api/src/main/java/com/fundtransfer/repository/UserRepository.java
@@ -1,12 +1,15 @@
 package com.fundtransfer.repository;
- 
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fundtransfer.entity.User;
- 
+
 public interface UserRepository extends JpaRepository<User, Long> {
- 
+
     User findByEmail(String email);
- 
+
+    Optional<User> findByAccountNumber(String accountNumber);
+
 }
- 
