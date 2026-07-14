2b0b476ae18224026016f211702d50caf51b9632
Bhavana <bachu.bhavana@accenture.com>
2026-07-14 16:13:39 +0530

    Implemented Profile page integration with backend API and fixed logged-in user data loading

--- fund-transfer-ui/src/app/components/profile/profile-details/profile-details.ts
+++ fund-transfer-ui/src/app/components/profile/profile-details/profile-details.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Router, RouterModule } from '@angular/router';
@@ -8,47 +8,68 @@ import { Router, RouterModule } from '@angular/router';
   standalone: true,
   imports: [
     CommonModule,
-    HttpClientModule,RouterModule
+    HttpClientModule,
+    RouterModule
   ],
   templateUrl: './profile-details.html',
   styleUrl: './profile-details.css'
 })
 export class ProfileDetails implements OnInit {
 
-  user:any={};
+  user: any = {};
 
-  constructor(private http:HttpClient, private router: Router){}
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private cdr: ChangeDetectorRef
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
 
-  const id = sessionStorage.getItem("userId");
-  console.log("USER ID =", id);
-  if (!id) {
-    console.log("User ID not found in session storage.");
-    return;
+    const id = sessionStorage.getItem("userId");
+
+    console.log("USER ID =", id);
+
+    if (!id) {
+      alert("User ID not found.");
+      return;
+    }
+
+    this.http.get<any>("http://localhost:8080/api/users/" + id)
+      .subscribe({
+
+        next: (data) => {
+
+          console.log("API DATA =", data);
+
+          this.user = data;
+
+          console.log("USER =", this.user);
+
+          //alert(this.user.name);
+
+          this.cdr.detectChanges();
+
+        },
+
+        error: (err) => {
+
+          console.log(err);
+
+          alert("Failed to load profile");
+
+        }
+
+      });
+
   }
-  this.http.get<any>("http://localhost:8080/api/users/" + id)
-    .subscribe({
 
-      next: (data) => {
-        console.log("API DATA =", data);
-        this.user = data;
-        console.log("USER =", this.user);
-      },
+  goToSetPin() {
+    this.router.navigate(['/set-pin']);
+  }
 
-      error: (err) => {
-        console.log(err);
-      }
-
-    });
-
-}
-      goToSetPin() {
-        this.router.navigate(['/set-pin']);
-      }
-      
-      goToChangePin() {
-        this.router.navigate(['/change-pin']);
-      }
+  goToChangePin() {
+    this.router.navigate(['/change-pin']);
+  }
 
 }
